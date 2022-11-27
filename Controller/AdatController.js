b3069e0a70fbfa6877b1e8fd1d2660d41678ba37
Bláj3n <ulrichbence15@freemail.hu>
2022-11-27 13:46:17 +0100

    alap script.js és AdatController

---
+++ Controller/AdatController.js
@@ -0,0 +1,7 @@
+class AdatController {
+    constructor() {
+
+    }
+}
+
+export default AdatController;
--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //itt lesz a program belépési pontja
+// a Controller fogja majd össze a Model-t és a View-t
+
+$(function () {
+    //itt példányosítjuk a Controllert
+});
