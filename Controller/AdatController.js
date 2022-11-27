f1b08d7f34bcb7735241e2c7b4986deb6efe00a3
Bláj3n <ulrichbence15@freemail.hu>
2022-11-27 17:50:15 +0100

    comment-kód igazítás

--- Controller/AdatController.js
+++ Controller/AdatController.js
@@ -5,15 +5,15 @@ import AdatView from "../View/AdatView.js";
 class AdatController {
     constructor() {
         //itt példányosítjuk a Modell-t és a View-t
-        //példányosítottunk egy View-t egy szülőelemmel, megkapta az adatokat és ezeket meg kell jelenítenie
         const adatModell = new AdatModell();
         const szuloelem = $("p");
+        //példányosítottunk egy View-t egy szülőelemmel, megkapta az adatokat és ezeket meg kell jelenítenie
         const adatView = new AdatView("alap", szuloelem);
         //itt írunk bármit a View-ba
         //itt hívjuk meg a Modell-t
         $("button").on("click", () => {
             let eredmeny = adatModell.setDuplicateErtek(); //visszatérési érték eltárolása
-            adatView.setErtek(eredmeny);
+            adatView.setErtek(eredmeny); //majd a View megjeleníti azt
             //itt írunk bármit a View-ba
             //itt hívjuk meg a Modell-t
         });
