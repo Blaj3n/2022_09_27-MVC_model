abbdd9aebaf7a504f308b9893f1a24267ca64d94
Bláj3n <ulrichbence15@freemail.hu>
2022-11-27 16:01:54 +0100

    setDuplicateErtek függvény

--- Modell/AdatModell.js
+++ Modell/AdatModell.js
@@ -1,6 +1,12 @@
 class AdatModell {
+    #ertek; //privát adattag
     constructor() {
-
+        this.#ertek = 22; //értékadás
+    }
+    //függvény ami, duplikálja az értéket
+    setDuplicateErtek() {
+        this.#ertek *= 2;
+        return this.#ertek;
     }
 }
 
