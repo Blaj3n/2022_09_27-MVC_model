cb4e2155293588b9fa86d55e7bd4f1d3fc287cbe
Bláj3n <ulrichbence15@freemail.hu>
2022-11-27 14:59:04 +0100

    AdatController importálása és példányosítása

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+import AdatController from "./Controller/AdatController.js"
 //itt lesz a program belépési pontja
 // a Controller fogja majd össze a Model-t és a View-t
 
 $(function () {
     //itt példányosítjuk a Controllert
+    new AdatController();
 });
